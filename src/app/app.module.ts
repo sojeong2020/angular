e8b35e83148d85fa011cf686a50a75f59e1de084
sojeong2020 <macpark09@gmail.com>
2022-02-21 23:18:55 +0000

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+
+
 
 
 import { HomeComponent } from './components/home/home.component';
@@ -20,6 +31,7 @@ import { EventsComponent } from './components/events/events.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { OppDetailComponent } from './components/opp-detail/opp-detail.component';
 import { DialogExampleComponent } from './components/dialog-example/dialog-example.component';
+import { FormFieldComponent } from './components/form-field/form-field.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +44,7 @@ import { DialogExampleComponent } from './components/dialog-example/dialog-examp
     CalendarComponent,
     OppDetailComponent,
     DialogExampleComponent,
+    FormFieldComponent,
   ],
   entryComponents: [DialogExampleComponent],
   imports: [
@@ -43,7 +56,18 @@ import { DialogExampleComponent } from './components/dialog-example/dialog-examp
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatNativeDateModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    MatCardModule,
+    MatRadioModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
